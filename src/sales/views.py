090f5da617c1d076eb3b4f4b5fef0a9377672f68
badar <badarul93@gmail.com>
2021-05-24 11:26:41 +0700

    24 Mei 2021

--- src/sales/views.py
+++ src/sales/views.py
@@ -6,22 +6,32 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Sale
 from .forms import SalesSearchForm
 import pandas as pd
+from .utils import get_customer_from_id, get_salesman_from_id, get_chart
 # Create your views here.
 
 def home_view(request):
     sales_df = None
     positions_df = None
+    merged_df = None
+    df = None
+    chart = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == "POST":
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        print(date_from, date_to, chart_type)
+        #print(date_from, date_to, chart_type)
 
         sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
         if len(sale_qs) > 0 :
             sales_df = pd.DataFrame(sale_qs.values())
+            sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
+            sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df['updated'] = sales_df['updated'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+            sales_df.rename({'customer_id':'customer', 'salesman_id':'salesman', 'id':'sales_id'}, axis=1, inplace=True)
             positions_data = []
             for sale in sale_qs:
                 for pos in sale.get_positions():
@@ -35,11 +45,16 @@ def home_view(request):
                     positions_data.append(obj)
 
             positions_df = pd.DataFrame(positions_data)
-            print('positions_df')
-            print(positions_df)
+            merged_df = pd.merge(sales_df, positions_df, on='sales_id')
 
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+            
+            chart = get_chart(chart_type, df, labels=df['transaction_id'].values)
+            print('chart', chart)
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
+            merged_df = merged_df.to_html()
+            df = df.to_html()
             print(sales_df)
         else:
             print('no data')
@@ -49,6 +64,9 @@ def home_view(request):
         'form': form,
         'sales_df': sales_df,
         'positions_df':positions_df,
+        'merged_df':merged_df,
+        'df':df,
+        'chart':chart,
     }
     return render(request, 'sales/home.html', context)
 
