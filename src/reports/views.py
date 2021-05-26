5b530d1fbd9a706cf76990c139146c653957685d
badar <badarul93@gmail.com>
2021-05-26 21:40:25 +0700

    new

--- src/reports/views.py
+++ src/reports/views.py
@@ -1,8 +1,11 @@
+from products.models import Product
 from typing import List
 from django.contrib import auth
 from django.core.checks import messages
 from django.forms.fields import JSONField
-from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView
 from matplotlib.pyplot import autoscale
 from profiles.models import Profile
 from django.http import JsonResponse
@@ -11,6 +14,16 @@ from .utils import get_report_image
 from .models import Report
 from .forms import ReportForm
 from django.views.generic import ListView, DetailView
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+from sales.models import Sale, Position, CSV
+from products.models import Product
+import csv
+from django.utils.dateparse import parse_date
 # Create your views here.
 
 class ReportListView(ListView):
@@ -20,6 +33,40 @@ class ReportListView(ListView):
 class ReportDetailView(DetailView):
     model = Report
     template_name = 'reports/detail.html'
+
+class UploadTemplateView(TemplateView):
+    template_name = 'reports/from_file.html'
+
+def csv_upload_view(request):
+    print('file is being send')
+
+    if request.method == 'POST':
+        csv_file = request.FILES.get('file')
+        obj = CSV.objects.create(file_name=csv_file)
+
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
+            reader.__next__()
+            for row in reader:
+                data = "".join(row)
+                data = data.split(',')
+                data.pop()
+
+                transaction_id = data[1]
+                product = data[2]
+                quantity = int(data[3])
+                customer = data[4]
+                date = parse_date(data[5])
+
+                try:
+                    product_obj = Product.objects.get(name=product)
+                except Product.DoesNotExist:
+                    product_obj = None
+
+                print(product_obj)
+
+
+    return HttpResponse()
 
 @login_required
 def create_report_view(request):
@@ -41,3 +88,26 @@ def create_report_view(request):
         #Report.objects.create(name=name, remarks=remarks, image=img, author=author)
         return JsonResponse({'msg':'send'})
     return JsonResponse({})
+
+@login_required
+def render_pdf_view(request, pk):
+    template_path = 'reports/pdf.html'
+    obj = get_object_or_404(Report, pk=pk)
+    context = {'obj': obj}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # If download 
+    #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # If display
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
